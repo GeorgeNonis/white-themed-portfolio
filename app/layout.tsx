8e27695a32a3b0ea26d0340c7e94c4b29d8057ea
GeorgeNonis <georgenonis@gmail.com>
2023-11-10 18:36:50 +0100

    colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50`}>{children}</body>
+      <body
+        className={`${inter.className}  bg-gray-50  relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+      >
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div></div>
+        {children}
+      </body>
     </html>
   );
 }
