a9d1376abfde99306d7728ba36a2bc452b722ed4
GeorgeNonis <georgenonis@gmail.com>
2023-11-11 22:21:52 +0100

    working on navbar

--- components/headers/index.tsx
+++ components/headers/index.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { links } from "@/config/data";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -7,7 +9,6 @@ const Header = () => {
       <motion.div
         className="
     fixed
-    -translate-x-1/2
     top-0
     left-1/2 
     h-[4.5rem] 
@@ -28,9 +29,29 @@ const Header = () => {
     dark:bg-gray-950 
     dark:border-black/40
     dark:bg-opacity-75"
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-      />
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      >
+        <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+          <ul className="flex  w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+            {links.map((link, index) => {
+              return (
+                <li
+                  key={index}
+                  className="h-3/4 flex items-center justify-center"
+                >
+                  <Link
+                    href={link.hash}
+                    className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </motion.div>
     </header>
   );
 };
