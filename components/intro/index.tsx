087da1902ff6962b560abc92b3680bfb499aae02
GeorgeNonis <georgenonis@gmail.com>
2023-11-12 21:15:21 +0100

    animation on portait and span

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -1,18 +1,41 @@
+"use client";
 import Image from "next/image";
 import MyImage from "@/public/me.png";
+import { motion } from "framer-motion";
 
 const Intro = () => {
   return (
     <section>
       <div className="flex items-center justify-center">
-        <div>
-          <Image
-            src={MyImage}
-            alt="my image"
-            width={"192"}
-            height={"192"}
-            quality={"95"}
-          />
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={MyImage}
+              alt="my image"
+              width={"192"}
+              height={"192"}
+              quality={"95"}
+              priority
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute text-4xl bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 2.7,
+            }}
+          >
+            👋
+          </motion.span>
         </div>
       </div>
     </section>
