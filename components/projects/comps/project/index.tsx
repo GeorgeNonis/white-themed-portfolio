3628badeb5f3738e7b7585601e63857f49fe6ae3
GeorgeNonis <georgenonis@gmail.com>
2023-11-13 23:53:06 +0100

    for now

--- components/projects/comps/project/index.tsx
+++ components/projects/comps/project/index.tsx
@@ -2,17 +2,37 @@
 
 import Image from "next/image";
 import { ProjectProps } from "./project.types";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 const Project = ({ description, imageUrl, tags, title }: ProjectProps) => {
-  const ref = useRef<HTMLPreElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  // Use the useScroll hook from Framer Motion to track the scroll progress.
+  // The target is the referenced element (ref), and the offset determines
+  // at what scroll positions (as a fraction of the viewport height) the scrollYProgress starts and ends.
   const { scrollYProgress } = useScroll({
-    target: tags,
+    target: ref,
     offset: ["0 1", "1.33 1"],
   });
+
+  // Use the useTransform hook to create a motion value that transforms the scrollYProgress
+  // (ranging from 0 to 1) into a scale value. This means when the scrollYProgress is at 0,
+  // the scale will be 0.8, and when scrollYProgress is at 1, the scale will be 1.
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+
+  // Similarly, useTransform is used here to create a motion value that transforms the
+  // scrollYProgress into an opacity value. When the scrollYProgress is at 0, the opacity will be 0.6,
+  // and when scrollYProgress is at 1, the opacity will be 1.
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
   return (
-    <motion.div className="group mb-3 sm:mb-8 last:mb-0">
+    <motion.div
+      className="group mb-3 sm:mb-8 last:mb-0"
+      ref={ref}
+      style={{
+        scale: scaleProgess,
+        opacity: opacityProgess,
+      }}
+    >
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative  hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
