595ceabe4682841bba42083e6a2722c813c3f141
GeorgeNonis <georgenonis@gmail.com>
2023-11-14 12:59:04 +0100

    added clsx in the project to use for active linkes in the navbar on top

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -10,7 +10,10 @@ import { FaGithubSquare } from "react-icons/fa";
 
 const Intro = () => {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+    <section
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      id="home"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
