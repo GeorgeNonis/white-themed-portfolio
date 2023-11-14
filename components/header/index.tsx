20c5aac2011505fe55f3961a38cd5a08c7a4981e
GeorgeNonis <georgenonis@gmail.com>
2023-11-14 13:03:46 +0100

    working

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -39,6 +39,7 @@ const Header = () => {
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex  w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link, index) => {
+            console.log(activeSection === link.name);
             return (
               <motion.li
                 key={index}
@@ -49,17 +50,28 @@ const Header = () => {
                 <Link
                   href={link.hash}
                   className={clsx(
-                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                    "relative flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                     {
                       "text-gray-950 dark:text-gray-200":
                         activeSection === link.name,
                     }
                   )}
                   onClick={() => {
-                    setActiveSection(link.hash);
+                    setActiveSection(link.name);
                   }}
                 >
                   {link.name}
+                  {link.name === activeSection && (
+                    <motion.span
+                      className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                      layoutId="activeSection"
+                      transition={{
+                        type: "spring",
+                        stiffness: 380,
+                        damping: 30,
+                      }}
+                    ></motion.span>
+                  )}
                 </Link>
               </motion.li>
             );
