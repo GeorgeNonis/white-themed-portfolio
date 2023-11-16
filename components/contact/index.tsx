1103d5e8bb4a1d37d195d4e78fe8da87ca47418e
GeorgeNonis <georgenonis@gmail.com>
2023-11-17 00:28:11 +0100

    structure

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,4 +1,21 @@
+import { useSectionInView } from "@/hooks";
+import SectionHeading from "../section-heading";
+
 const Contact = () => {
-  return <div></div>;
+  const { ref } = useSectionInView("Contact");
+  return (
+    <section id="contact" ref={ref}>
+      <SectionHeading>Contact Me</SectionHeading>
+      <p>
+        Please contact me directly at
+        <br />
+        <a className="underline" href="mailto:georgenonis@gmail.com">
+          georgenonis@gmail.com
+        </a>
+        <br />
+        or through this form.
+      </p>
+    </section>
+  );
 };
 export default Contact;
