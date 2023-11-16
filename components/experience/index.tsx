ce7c354d7c3279f96de63c7d4e25164b99268aff
GeorgeNonis <georgenonis@gmail.com>
2023-11-16 14:42:10 +0100

    looks cool

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useSectionInView } from "@/hooks";
 import SectionHeading from "../section-heading";
 import {
@@ -8,13 +10,13 @@ import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/config/data";
 
 const Experience = () => {
-  const { ref } = useSectionInView("Experience");
+  const { ref } = useSectionInView("Experience", 0.2);
   return (
     <section id="Experience" ref={ref}>
       <SectionHeading>My Experience</SectionHeading>
       <VerticalTimeline>
         {experiencesData.map((experience, index) => (
-          <VerticalTimelineElement key={index}>
+          <VerticalTimelineElement key={index} icon={experience.icon}>
             <h3 className="font-semibold capitalize">{experience.title}</h3>
             <p className="font-normal !mt-0">{experience.location}</p>
             <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
