09f39f872806b78ac4123d1119dd590f415f0e55
GeorgeNonis <georgenonis@gmail.com>
2023-11-16 14:54:33 +0100

    styles

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -10,16 +10,34 @@ import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/config/data";
 
 const Experience = () => {
-  const { ref } = useSectionInView("Experience", 0.2);
+  const { ref } = useSectionInView("Experience");
   return (
-    <section id="Experience" ref={ref}>
+    <section id="Experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My Experience</SectionHeading>
       <VerticalTimeline>
         {experiencesData.map((experience, index) => (
-          <VerticalTimelineElement key={index} icon={experience.icon}>
+          <VerticalTimelineElement
+            key={index}
+            contentStyle={{
+              background: "#f3f4f6",
+              boxShadow: "none",
+              border: "1px solid rgba(0, 0, 0, 0.05)",
+              textAlign: "left",
+              padding: "1.3rem 2rem",
+            }}
+            contentArrowStyle={{
+              borderRight: "0.4rem solid #9ca3af",
+            }}
+            icon={experience.icon}
+            date={experience.date}
+            iconStyle={{
+              background: "white",
+              fontSize: "1.5rem",
+            }}
+          >
             <h3 className="font-semibold capitalize">{experience.title}</h3>
             <p className="font-normal !mt-0">{experience.location}</p>
-            <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+            <p className="mt-1 font-normal text-gray-700 dark:text-white/75">
               {experience.description}
             </p>
           </VerticalTimelineElement>
