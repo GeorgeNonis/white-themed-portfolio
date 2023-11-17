fcabbd41f37415643c813a0e6260d821d336c802
GeorgeNonis <georgenonis@gmail.com>
2023-11-17 17:24:09 +0100

    testing resend package

--- components/contact/action.ts
+++ components/contact/action.ts
@@ -1,7 +1,16 @@
 "use server";
+import React from "react";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 export const formHandler = async (formData: FormData) => {
   const senderEmail = formData.get("email");
-  const message = formData.get("message");
+  const message = formData.get("message") || "";
 
-  console.log({ senderEmail, message });
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "georgenonis@gmail.com",
+    subject: "Message from portfolio contact form",
+    text: "Testing",
+  });
 };
