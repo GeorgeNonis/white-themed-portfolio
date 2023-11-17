e76de73267cb35644e181e0ae38567486d3819e0
GeorgeNonis <georgenonis@gmail.com>
2023-11-17 02:11:05 +0100

    styles

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -2,17 +2,31 @@
 
 import { useSectionInView } from "@/hooks";
 import SectionHeading from "../section-heading";
+import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   const { ref } = useSectionInView("Contact");
   return (
-    <section
+    <motion.section
       id="contact"
       ref={ref}
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0.2,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
       <SectionHeading>Contact Me</SectionHeading>
-      <p>
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at
         <br />
         <a className="underline" href="mailto:georgenonis@gmail.com">
@@ -21,12 +35,21 @@ const Contact = () => {
         <br />
         or through this form.
       </p>
-      <form>
-        <input type="email" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <button type="submit">Sumbit</button>
+      <form className="mt-10 flex flex-col dark:text-black">
+        <input
+          type="email"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+        />
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"></textarea>
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
+        >
+          Sumbit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+        </button>
       </form>
-    </section>
+    </motion.section>
   );
 };
 export default Contact;
