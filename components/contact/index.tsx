9a8a3cd4f712040d750430e6b1655995a126dffd
GeorgeNonis <georgenonis@gmail.com>
2023-11-17 02:11:19 +0100

    opacity

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -13,13 +13,13 @@ const Contact = () => {
       ref={ref}
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
       initial={{
-        opacity: 0.2,
+        opacity: 0,
       }}
       whileInView={{
         opacity: 1,
       }}
       transition={{
-        duration: 1.5,
+        duration: 1,
       }}
       viewport={{
         once: true,
@@ -46,7 +46,7 @@ const Contact = () => {
           className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
         >
           Sumbit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+          <FaPaperPlane className=" opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </button>
       </form>
     </motion.section>
