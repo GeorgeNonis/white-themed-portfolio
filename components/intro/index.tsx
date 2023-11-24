d683cd886011ea841bde343dbceec39086a76b29
GeorgeNonis <georgenonis@gmail.com>
2023-11-25 00:09:04 +0100

    change linkedin address

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -96,7 +96,7 @@ const Intro = () => {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/george-nonis-697430234/"
+          href="https://www.linkedin.com/in/george-nonis/"
           target="_blank"
         >
           <BsLinkedin />
